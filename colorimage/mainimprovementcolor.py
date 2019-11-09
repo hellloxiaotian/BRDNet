1845be29f96f87ed037b6a1e3e54c8f0cf460a91
chunweitian <chunweitian@163.com>
2019-11-09 15:10:25 +0800

    first commit

---
+++ colorimage/mainimprovementcolor.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import os, time, glob
+import PIL.Image as Image
+import numpy as np
+import pandas as pd
+#from keras import backend as K
+import tensorflow as tf
+from keras.callbacks import CSVLogger, ModelCheckpoint, LearningRateScheduler
+from keras.models import load_model
+from keras.optimizers import Adam
+from skimage.measure import compare_psnr, compare_ssim
+import models 
+from multiprocessing import Pool
+import random
+## Params
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', default='BRDNet', type=str, help='choose a type of model')
+parser.add_argument('--batch_size', default=20, type=int, help='batch size') #128
+parser.add_argument('--train_data', default='./data/3859waterloo5050step40color1/', type=str, help='path of train data') #201807081928tcw
+parser.add_argument('--test_dir', default='./data/Test/Kodak24', type=str, help='directory of test dataset')
+parser.add_argument('--sigma',default = 75, type =int,help='noise level')
+parser.add_argument('--epoch', default=50, type=int, help='number of train epoches')
+parser.add_argument('--lr', default=1e-3, type=float, help='initial learning rate for Adam')
+parser.add_argument('--save_every', default=5, type=int, help='save model at every x epoches') #every x epoches save model
+parser.add_argument('--pretrain', default=None, type=str, help='path of pre-trained model')
+parser.add_argument('--only_test', default=False, type=bool, help='train and test or only test')
+args = parser.parse_args()
+
+if not args.only_test:
+    save_dir = './snapshot/save_'+ args.model + '_' + 'sigma' + str(args.sigma) + '_' + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + '/'
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    # log
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%Y %H:%M:%S',
+                    filename=save_dir+'info.log',
+                    filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-6s: %(levelname)-6s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    
+    logging.info(args)
+    
+else:
+    save_dir = '/'.join(args.pretrain.split('/')[:-1]) + '/'
+
+
+def step_decay(epoch):
+    
+    initial_lr = args.lr
+    if epoch<30:   #tcw
+        #lr = initial_lr/10 #tcw
+         lr= initial_lr
+    else:
+        lr = initial_lr/10
+    
+    return lr
+
+
+def train_datagen(y_, batch_size=8): #201807081925tcw
+    while(True):
+	    for i in range(0,len(y_),batch_size):
+		img1 = []
+		for j in range(i,min(i+batch_size,len(y_))): #read batchsize images, which uses to train
+		   img = np.array(Image.open(y_[j]),dtype='uint8') #dtype='float32')/255.0#.convert('L') #tcw 2018041
+		   img1.append(img)
+		get_batch_y = img1
+		get_batch_y= np.array(get_batch_y)
+                get_batch_y = get_batch_y.astype('float32')/255.0
+		get_batch_y = get_batch_y.reshape(get_batch_y.shape[0], get_batch_y.shape[1], get_batch_y.shape[2], 3) # If the last parameter is 1, the image is gray. If the last parameter is 3, the image is color image.  201807082123tcw
+	#	np.random.shuffle(get_batch_y)
+		noise =  np.random.normal(0, args.sigma/255.0, get_batch_y.shape)    # noise
+		get_batch_x = get_batch_y + noise  # input image = clean image + noise
+		yield get_batch_x, get_batch_y
+
+#201807081928tcw
+def load_images(data_path):
+    images = [];
+    file_dictory1 = glob.glob(args.train_data+'*.bmp') #notice the data format
+    for file in file_dictory1:
+        #print file
+         images.append(file)
+    random.shuffle(images) 
+    return images
+def train():
+    images = load_images(args.train_data)
+    # model selection
+    if args.pretrain:   model = load_model(args.pretrain, compile=False) 
+    else:   
+        if args.model == 'BRDNet': model = models.BRDNet() #orginal format tcw 20180429
+    # compile the model
+    model.compile(optimizer=Adam(), loss=['mse'])
+    # use call back functions
+    ckpt = ModelCheckpoint(save_dir+'/model_{epoch:02d}.h5', monitor='val_loss', 
+                    verbose=0, period=args.save_every)
+    csv_logger = CSVLogger(save_dir+'/log.csv', append=True, separator=',')
+    lr = LearningRateScheduler(step_decay)
+    history = model.fit_generator(train_datagen(images, batch_size=args.batch_size),
+                    steps_per_epoch=len(images)//args.batch_size, epochs=args.epoch, verbose=1, 
+                    callbacks=[ckpt, csv_logger, lr])
+    return model
+
+def test(model):
+    
+    print('Start to test on {}'.format(args.test_dir))
+    out_dir = save_dir + args.test_dir.split('/')[-1] + '/'
+    if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+            
+    name = []
+    psnr = []
+    ssim = []
+    file_list = glob.glob('{}/*'.format(args.test_dir)) #notice: it is easy to generate error  $201804101000tcw #notice; need to change the format
+    for file in file_list:
+        # read image
+        img_clean = np.array(Image.open(file), dtype='float32') / 255.0
+        np.random.seed(0) #obtain the same random data when it is in the test phase tcw201804151350
+        img_test = img_clean + np.random.normal(0, args.sigma/255.0, img_clean.shape)
+        img_test = img_test.astype('float32')
+        # predict
+        x_test = img_test.reshape(1, img_test.shape[0], img_test.shape[1], 3) #if the last parameter is 1, the image is gray. If the last parameter is 3201807082123tcw
+        y_predict = model.predict(x_test) #tcw
+        # calculate numeric metrics
+        img_out = y_predict.reshape(img_clean.shape)
+        img_out = np.clip(img_out, 0, 1)
+        psnr_noise, psnr_denoised = compare_psnr(img_clean, img_test,True), compare_psnr(img_clean, img_out,True)
+        ssim_noise, ssim_denoised = compare_ssim(img_clean, img_test,multichannel=True), compare_ssim(img_clean, img_out,multichannel=True)
+        psnr.append(psnr_denoised)
+        ssim.append(ssim_denoised)
+        # save images
+        filename = file.split('/')[-1].split('.')[0]    # get the name of image file
+        name.append(filename)
+        img_test = Image.fromarray((img_test*255).astype('uint8'))
+        img_test.save(out_dir+filename+'_sigma'+'{}_psnr{:.2f}.png'.format(args.sigma, psnr_noise))
+        img_out = Image.fromarray((img_out*255).astype('uint8')) 
+        img_out.save(out_dir+filename+'_psnr{:.2f}.png'.format(psnr_denoised))
+    #    print psnr_denoised
+    #	print len(psnr)
+        #print sum(psnr)
+    psnr_avg = sum(psnr)/len(psnr)
+    ssim_avg = sum(ssim)/len(ssim)
+    name.append('Average')
+    psnr.append(psnr_avg)
+    ssim.append(ssim_avg)
+    print('Average PSNR = {0:.2f}, SSIM = {1:.2f}'.format(psnr_avg, ssim_avg))
+    
+    pd.DataFrame({'name':np.array(name), 'psnr':np.array(psnr), 'ssim':np.array(ssim)}).to_csv(out_dir+'/metrics.csv', index=True)
+    
+if __name__ == '__main__':   
+    
+    if args.only_test:
+        model = load_model(args.pretrain, compile=False)
+        test(model)
+    else:
+        model = train()
+        test(model)       
+    
---
+++ grayimage/models.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+#import tensorflow as tf
+from keras.models import *
+from keras.layers import  Input,Conv2D,BatchNormalization,Activation,Lambda,Subtract,concatenate,Add,merge
+import keras.backend  as K
+#from group_norm import GroupNormalization #
+from batch_renorm import BatchRenormalization
+def BRDNet(): #original format def BRDNet(), data is used to obtain the reshape of input data
+    inpt = Input(shape=(None,None,1)) #if the image is 3, it is color image. If the image is 1, it is gray color, 201807082123tcw
+    # 1st layer, Conv+relu
+    x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(inpt)
+    x = BatchRenormalization(axis=-1, epsilon=1e-3)(x)
+    x = Activation('relu')(x)
+    # 15 layers, Conv+BN+relu
+    for i in range(7):
+        x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(x)
+        x = BatchRenormalization(axis=-1, epsilon=1e-3)(x)
+        x = Activation('relu')(x)   
+    # last layer, Conv 
+    for i in range(8):
+        x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(x)
+        x = BatchRenormalization(axis=-1, epsilon=1e-3)(x)
+        x = Activation('relu')(x) 
+    x = Conv2D(filters=1, kernel_size=(3,3), strides=(1,1), padding='same')(x) #gray is 1 color is 3
+    x = Subtract()([inpt, x])   # input - noise
+    y = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(inpt)
+    y = BatchRenormalization(axis=-1, epsilon=1e-3)(y)
+    y = Activation('relu')(y)
+    # 15 layers, Conv+BN+relu
+    for i in range(7):
+        y = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1),dilation_rate=(2,2), padding='same')(y)
+        y = Activation('relu')(y)   
+    y = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(y)
+    y = BatchRenormalization(axis=-1, epsilon=1e-3)(y)
+    y = Activation('relu')(y) 
+    for i in range(6):
+        y = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1),dilation_rate=(2,2), padding='same')(y)
+        y = Activation('relu')(y)
+    y = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(y)
+    y = BatchRenormalization(axis=-1, epsilon=1e-3)(y)
+    y = Activation('relu')(y)    
+    y = Conv2D(filters=1, kernel_size=(3,3), strides=(1,1), padding='same')(y)#gray is 1 color is 3
+    y = Subtract()([inpt, y])   # input - noise
+    o = concatenate([x,y],axis=-1)
+    z = Conv2D(filters=1, kernel_size=(3,3), strides=(1,1), padding='same')(o)#gray is 1 color is 3
+    z=  Subtract()([inpt, z])
+    model = Model(inputs=inpt, outputs=z)
+    return model
+   
+
+
+
+
